fa4cf8b970be21e89c2e970548988993182e689a
天机否 <1171601618@qq.com>
2017-03-24 15:29:40 +0800

    Initial Commit

---
+++ TestSort/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSort
+//
+//  Created by 天机否 on 17/3/24.
+//  Copyright © 2017年 tianjifou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TestSortTests/TestSortTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestSortTests.swift
+//  TestSortTests
+//
+//  Created by 天机否 on 17/3/24.
+//  Copyright © 2017年 tianjifou. All rights reserved.
+//
+
+import XCTest
+@testable import TestSort
+
+class TestSortTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
